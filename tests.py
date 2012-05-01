79d9cf29a795b986d59bde001784eae476b282e1
Randall Degges <rdegges@gmail.com>
2012-04-30 17:39:56 -0700

    Adding tests for decode.

--- tests.py
+++ tests.py
@@ -22,3 +22,39 @@ class Encode(TestCase):
 
     def test_encodes_long_password(self):
         self.assertTrue(encode('', 'password'*1000000))
+
+
+class Decode(TestCase):
+
+    def test_decodes_empty_username(self):
+        self.assertEqual('', decode(encode('', 'password'))[0])
+
+    def test_decodes_empty_password(self):
+        self.assertEqual('', decode(encode('username', ''))[1])
+
+    def test_decodes_hashes_only(self):
+        username, password = 'username', 'omgawesome!'
+        encoded_str = encode(username, password)
+        encoded_hash = encoded_str.split(' ')[1]
+        self.assertEqual((username, password), decode(encoded_hash))
+
+    def test_decodes_fully_encoded_strings(self):
+        username, password = 'username', 'password'
+        encoded_str = encode(username, password)
+        self.assertEqual((username, password), decode(encoded_str))
+
+    def test_doesnt_decode_invalid_auth_types(self):
+        encoded_str = 'error woot'
+        self.assertRaises(DecodeError, decode, encoded_str)
+
+    def test_doesnt_decode_invalid_hashes(self):
+        encoded_str = 'basic omg hacks what'
+        self.assertRaises(DecodeError, decode, encoded_str)
+
+        encoded_str = 'basic omg hacks'
+        self.assertRaises(DecodeError, decode, encoded_str)
+
+    def test_properly_escapes_colons(self):
+        username, password = 'user:name:', 'pass:word:'
+        encoded_str = encode(username, password)
+        self.assertEqual((username, password), decode(encoded_str))
