95cc4398588f236bc691cfa34cffce8a16c0028b
Randall Degges <r@rdegges.com>
2022-12-20 22:10:39 -0800

    Changing my email.
    It's changed over the years =)

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
 
     # Metadata for PyPI:
     author='Randall Degges',
-    author_email='rdegges@gmail.com',
+    author_email='r@rdegges.com',
     license='UNLICENSE',
     url='https://github.com/rdegges/python-basicauth',
     keywords='python security basicauth http',
