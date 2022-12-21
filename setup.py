f6080c299304afa32a1af0fcbf27288e78a3d946
Randall Degges <r@rdegges.com>
2022-12-21 10:56:30 -0800

    Updating trove classifiers.
    Notable changes:
    
    - Dropping Python 2.6 support and all EOL'd Python versions
    - Adding in some additional classifiers for thoroughness' sake

--- setup.py
+++ setup.py
@@ -14,16 +14,21 @@ setup(
     include_package_data=True,
 
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
         'License :: Public Domain',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
     ],
 
     # Package dependencies:
